0f0c1c64aaafa451d08ce4a78d7161926d39cad0
Sigmanor <sigmanor@gmail.com>
2016-01-07 17:03:01 +0200

    Changed app version

--- EncryptPad/Properties/AssemblyInfo.cs
+++ EncryptPad/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
