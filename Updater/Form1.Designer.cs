33a933d0143b088e9c7130e4030ba4b2764239c7
Sigmanor <sigmanor@gmail.com>
2016-01-14 22:32:57 +0200

    Added updater

---
+++ Updater/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Updater
+{
+    partial class UpdateForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(76, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Application will be updated and reopened.";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(294, 83);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::Updater.Properties.Resources.inbox_download;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // UpdateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 121);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UpdateForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crypto Notepad Updater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Updater/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Ionic.Zip;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Net;
+using System.Reflection;
+
+namespace Updater
+{
+    public partial class UpdateForm : Form
+    {
+        string[] arg;
+        string exePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"\";
+        bool okButton = false;
+
+        public UpdateForm(string[] args)
+        {
+            arg = args;
+            InitializeComponent();
+        }
+
+        #region ExtractZip
+        public void ExtractFileToDirectory(string zipFileName, string outputDirectory)
+        {
+            ZipFile zip = ZipFile.Read(zipFileName);
+            Directory.CreateDirectory(outputDirectory);
+            zip.ExtractAll(outputDirectory, ExtractExistingFileAction.OverwriteSilently);
+        }
+        #endregion
+
+        void downloader_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                System.Media.SystemSounds.Beep.Play();
+                okButton = true;
+                button1.Enabled = true;
+                label1.Text = "There was some errors during the update, please try again later.";
+            }
+
+            if (e.Error == null)
+            {
+                var pr = new Process();
+                ExtractFileToDirectory(exePath + "Crypto-Notepad-Update.zip", exePath);
+                pr.StartInfo.FileName = exePath + "Crypto Notepad.exe";
+                pr.Start();
+                Application.Exit();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (arg.Length == 0)
+            {
+                this.Close();
+            }
+
+            else if (arg[0] == "/u")
+            {
+                return;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (okButton == false)
+            {
+                var pr = new Process();
+                button1.Enabled = false;
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(downloader_DownloadFileCompleted);
+                webClient.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/Sigmanor/Crypto-Notepad/master/Crypto-Notepad-Update.zip"), exePath + "Crypto-Notepad-Update.zip");
+            }
+
+            if (okButton == true)
+            {
+                var pr = new Process();
+                pr.StartInfo.FileName = exePath + "Crypto Notepad.exe";
+                pr.Start();
+                Application.Exit();
+            }
+        }
+
+
+    }
+}
