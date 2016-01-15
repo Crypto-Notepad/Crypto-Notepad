7bfece31cc865f6bd0b55ad9214eb8026402b95f
Sigmanor <sigmanor@gmail.com>
2016-01-15 19:48:48 +0200

    Small improvements in design

--- Crypto Notepad/AboutFrom.Designer.cs
+++ Crypto Notepad/AboutFrom.Designer.cs
@@ -35,7 +35,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.customRichTextBox1 = new Crypto_Notepad.CustomRichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -71,9 +70,10 @@
             this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(6, 33);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(215, 32);
+            this.label2.Size = new System.Drawing.Size(215, 67);
             this.label2.TabIndex = 2;
-            this.label2.Text = "This is an open-source app created by          and distributed under";
+            this.label2.Text = "This is an open-source app created by          and distributed under\r\nThe source " +
+    "code, issues and documentation posted on";
             // 
             // label3
             // 
@@ -81,7 +81,7 @@
             this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label3.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(207, 45);
+            this.label3.Location = new System.Drawing.Point(207, 46);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(25, 13);
             this.label3.TabIndex = 3;
@@ -93,7 +93,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(6, 103);
+            this.label5.Location = new System.Drawing.Point(6, 98);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(182, 15);
             this.label5.TabIndex = 4;
@@ -105,22 +105,12 @@
             this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label6.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(147, 78);
+            this.label6.Location = new System.Drawing.Point(147, 72);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(43, 13);
             this.label6.TabIndex = 5;
             this.label6.Text = "GitHub";
             this.label6.Click += new System.EventHandler(this.label6_Click);
-            // 
-            // label7
-            // 
-            this.label7.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(6, 65);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(219, 28);
-            this.label7.TabIndex = 2;
-            this.label7.Text = "The source code, issues and documentation posted on";
             // 
             // label4
             // 
@@ -128,7 +118,7 @@
             this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label4.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(23, 45);
+            this.label4.Location = new System.Drawing.Point(24, 46);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(55, 13);
             this.label4.TabIndex = 3;
@@ -141,7 +131,7 @@
             this.customRichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.customRichTextBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.customRichTextBox1.ForeColor = System.Drawing.Color.White;
-            this.customRichTextBox1.Location = new System.Drawing.Point(0, 125);
+            this.customRichTextBox1.Location = new System.Drawing.Point(0, 120);
             this.customRichTextBox1.Name = "customRichTextBox1";
             this.customRichTextBox1.ReadOnly = true;
             this.customRichTextBox1.RightMargin = 600;
@@ -159,12 +149,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(50)))));
-            this.ClientSize = new System.Drawing.Size(299, 213);
+            this.ClientSize = new System.Drawing.Size(300, 208);
             this.Controls.Add(this.customRichTextBox1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
@@ -193,7 +182,6 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private CustomRichTextBox customRichTextBox1;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label4;
     }
 }
