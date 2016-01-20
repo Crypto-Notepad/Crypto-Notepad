6f4324cc78b300ac10bdc7db15d43e8294dfce33
Sigmanor <sigmanor@gmail.com>
2016-01-20 21:24:04 +0200

    Disabling Accept button when the textboxes are empty

--- Crypto Notepad/ChangeKeyForm.Designer.cs
+++ Crypto Notepad/ChangeKeyForm.Designer.cs
@@ -50,6 +50,7 @@
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
             this.textBox1.UseSystemPasswordChar = true;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -58,6 +59,7 @@
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 1;
             this.textBox2.UseSystemPasswordChar = true;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label1
             // 
@@ -79,6 +81,7 @@
             // 
             // button1
             // 
+            this.button1.Enabled = false;
             this.button1.Location = new System.Drawing.Point(182, 82);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
--- Crypto Notepad/ChangeKeyForm.cs
+++ Crypto Notepad/ChangeKeyForm.cs
@@ -66,5 +66,30 @@ namespace Crypto_Notepad
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0 & textBox2.Text.Length > 0)
+            {
+                button1.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0 & textBox2.Text.Length > 0)
+            {
+                button1.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
+        }
+
     }
 }
