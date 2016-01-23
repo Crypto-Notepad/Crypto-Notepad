f619cc6777ccb28fbffa32fbb97c154f9232d87c
Sigmanor <sigmanor@gmail.com>
2016-01-23 21:20:23 +0200

    Version update

--- Crypto Notepad/Properties/AssemblyInfo.cs
+++ Crypto Notepad/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.1.0")]
-[assembly: AssemblyFileVersion("0.6.1.0")]
+[assembly: AssemblyVersion("0.6.2.0")]
+[assembly: AssemblyFileVersion("0.6.2.0")]
