a3ba2a221db2fd3293f7d433a0d87224f39431d7
Alexander <sigmanor@gmail.com>
2018-12-06 13:14:03 +0200

    Removed unused code

--- Crypto Notepad/AboutFrom.cs
+++ Crypto Notepad/AboutFrom.cs
@@ -11,36 +11,10 @@ namespace Crypto_Notepad
             InitializeComponent();
         }
 
-        private void label7_Click(object sender, EventArgs e)
-        {
-            Process.Start("https://github.com/Sigmanor/Crypto-Notepad");
-        }
-
-        private void label9_Click(object sender, EventArgs e)
-        {
-            Process.Start("http://www.codeproject.com/Articles/20917/Creating-a-Custom-Settings-Provider");
-        }
-
-        private void label11_Click(object sender, EventArgs e)
-        {
-            Process.Start("http://easylab.net.ua/net-c-windows-forms/c-algoritm-shifrovaniya-aes");
-        }
-
-        private void label13_Click(object sender, EventArgs e)
-        {
-            Process.Start("https://code.msdn.microsoft.com/windowsdesktop/How-to-find-the-text-and-e92b8d78");
-        }
-
         private void AboutWindow_Load(object sender, EventArgs e)
         {
             Version vrs = new Version(Application.ProductVersion);
             label1.Text = string.Format("Crypto Notepad " + "{0}.{1}.{2}", vrs.Major, vrs.Minor, vrs.Build);
-        }
-
-        private void label14_Click(object sender, EventArgs e)
-        {
-            Process.Start("http://stackoverflow.com/a/2576220/4430027");
-
         }
 
         private void customRichTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
