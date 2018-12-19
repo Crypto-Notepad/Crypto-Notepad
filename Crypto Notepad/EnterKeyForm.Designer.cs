ac322b1e47cfe87b8533c167da28a9fbe26e3e2c
Alexander <sigmanor@gmail.com>
2018-12-20 01:07:35 +0200

    Minor button location changes

--- Crypto Notepad/EnterKeyForm.Designer.cs
+++ Crypto Notepad/EnterKeyForm.Designer.cs
@@ -54,7 +54,7 @@
             // button1
             // 
             this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(116, 79);
+            this.button1.Location = new System.Drawing.Point(113, 79);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
