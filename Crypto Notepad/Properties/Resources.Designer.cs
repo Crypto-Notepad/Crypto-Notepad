81538994271f6a2aae8e414e8c3b8d4c178add55
Alexander <sigmanor@gmail.com>
2019-10-20 21:29:53 +0300

    Resolved #38

--- Crypto Notepad/Properties/Resources.Designer.cs
+++ Crypto Notepad/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Crypto_Notepad.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap application {
+            get {
+                object obj = ResourceManager.GetObject("application", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap applications_blue {
             get {
                 object obj = ResourceManager.GetObject("applications_blue", resourceCulture);
