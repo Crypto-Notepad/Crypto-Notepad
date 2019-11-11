598006d735d8abd23a18024d8883cb234a6e2858
Alexander <sigmanor@gmail.com>
2019-11-11 19:21:58 +0200

    Added helper class

---
+++ Crypto Notepad/WinFormsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Crypto_Notepad
+{
+    public static class WinFormsExtensions
+    {
+        public static void AppendLine(this TextBox source, string value)
+        {
+            if (source.Text.Length == 0)
+                source.Text = value;
+            else
+                source.AppendText("\r\n" + value);
+        }
+
+        public static void Times(this int count, Action action)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                action();
+            }
+        }
+    }
+}
