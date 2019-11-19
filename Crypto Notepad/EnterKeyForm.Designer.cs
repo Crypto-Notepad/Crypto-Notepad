0a50567b05ed1d985e0495e403b834f39cc67a44
Alexander <sigmanor@gmail.com>
2019-11-19 15:57:15 +0200

    dev

--- Crypto Notepad/EnterKeyForm.Designer.cs
+++ Crypto Notepad/EnterKeyForm.Designer.cs
@@ -32,15 +32,15 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EnterKeyForm));
             this.okButton = new System.Windows.Forms.Button();
             this.mainPanel = new System.Windows.Forms.Panel();
-            this.lockPictureBox = new System.Windows.Forms.PictureBox();
             this.fileNameLabel = new System.Windows.Forms.Label();
             this.showKeyPictureBox = new System.Windows.Forms.PictureBox();
+            this.lockPictureBox = new System.Windows.Forms.PictureBox();
             this.enterKeyFormToolTip = new System.Windows.Forms.ToolTip(this.components);
             this.passwordGeneratorButton = new System.Windows.Forms.Button();
             this.keyTextBox = new System.Windows.Forms.PlaceholderTextBox();
             this.mainPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.lockPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.showKeyPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lockPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // okButton
@@ -67,24 +67,12 @@
             this.mainPanel.Size = new System.Drawing.Size(295, 75);
             this.mainPanel.TabIndex = 5;
             // 
-            // lockPictureBox
-            // 
-            this.lockPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lockPictureBox.Cursor = System.Windows.Forms.Cursors.Default;
-            this.lockPictureBox.Image = global::Crypto_Notepad.Properties.Resources.key_solid;
-            this.lockPictureBox.Location = new System.Drawing.Point(8, 14);
-            this.lockPictureBox.Name = "lockPictureBox";
-            this.lockPictureBox.Size = new System.Drawing.Size(47, 47);
-            this.lockPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.lockPictureBox.TabIndex = 6;
-            this.lockPictureBox.TabStop = false;
-            // 
             // fileNameLabel
             // 
             this.fileNameLabel.AutoEllipsis = true;
             this.fileNameLabel.Location = new System.Drawing.Point(61, 14);
             this.fileNameLabel.Name = "fileNameLabel";
-            this.fileNameLabel.Size = new System.Drawing.Size(196, 13);
+            this.fileNameLabel.Size = new System.Drawing.Size(221, 13);
             this.fileNameLabel.TabIndex = 6;
             this.fileNameLabel.Text = "File name";
             this.fileNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -102,6 +90,18 @@
             this.showKeyPictureBox.TabIndex = 3;
             this.showKeyPictureBox.TabStop = false;
             this.showKeyPictureBox.Click += new System.EventHandler(this.ShowKeyPictureBox_Click);
+            // 
+            // lockPictureBox
+            // 
+            this.lockPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lockPictureBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lockPictureBox.Image = global::Crypto_Notepad.Properties.Resources.key_solid;
+            this.lockPictureBox.Location = new System.Drawing.Point(8, 14);
+            this.lockPictureBox.Name = "lockPictureBox";
+            this.lockPictureBox.Size = new System.Drawing.Size(47, 47);
+            this.lockPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.lockPictureBox.TabIndex = 6;
+            this.lockPictureBox.TabStop = false;
             // 
             // passwordGeneratorButton
             // 
@@ -142,14 +142,14 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "EnterKeyForm";
-            this.Text = "Crypto Notepad";
+            this.Text = "Enter encryption key";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.EnterKeyForm_FormClosed);
             this.Load += new System.EventHandler(this.EnterKeyForm_Load);
             this.Shown += new System.EventHandler(this.EnterKeyForm_Shown);
             this.mainPanel.ResumeLayout(false);
             this.mainPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.lockPictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.showKeyPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lockPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
